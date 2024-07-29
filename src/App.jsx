ec8b752c579848b41e4f47797f4c9748b43c829b
Ebubekir Tas <tas_ebubekir@yahoo.com>
2024-07-29 13:16:05 -0400

    weapon respawn timer util

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import axios from "axios";
 
 function App() {
@@ -16,6 +16,44 @@ function App() {
   const [isSaveDisabled, setIsSaveDisabled] = useState(false);
 
   const [isFetching, setIsFetching] = useState(false);
+
+  const weaponsData = useMemo(() => {
+    const wsMatch = mapData.match(/&ws=\s*([\s\S]+?)\s*&rt=/);
+    if (!wsMatch) return [];
+
+    const wsSection = wsMatch[1];
+
+    const wsArray = [];
+    for (let i = 0; i < wsSection.length; i += 4) {
+      wsArray.push(wsSection.substr(i, 4));
+    }
+
+    const weapons = [];
+    for (let i = 0; i < wsArray.length; i += 5) {
+      const weapon = wsArray.slice(i, i + 5);
+      weapons.push(weapon);
+    }
+
+    return weapons;
+  }, [mapData]);
+
+  const handleMapDataChange = (index, newValue) => {
+    const value = Math.max(0, Math.min(30, parseInt(newValue, 10)));
+    const formattedValue = String(value).padEnd(4, " ");
+
+    const updatedWeapons = [...weaponsData];
+    updatedWeapons[index][4] = formattedValue;
+
+    const updatedWsArray = updatedWeapons.flat();
+
+    const newMapData = mapData.replace(
+      /(&ws=)\s*([\s\S]+?)\s*(&rt=)/,
+      (_, prefix, wsSection, suffix) =>
+        `${prefix} ${updatedWsArray.join("")}${suffix}`
+    );
+
+    setMapData(newMapData);
+  };
 
   const handleLoad = async () => {
     try {
@@ -35,8 +73,7 @@ function App() {
       setSaveMapSlotNum(mapSlotNum);
     } catch (error) {
       alert("Map Load Failed");
-    }
-    finally {
+    } finally {
       setIsFetching(false);
     }
   };
@@ -54,7 +91,6 @@ function App() {
           server: saveServer,
         }
       );
-      console.log(response.data);
       if (response.data) {
         alert(
           "successfully saved map to user: " +
@@ -84,98 +120,132 @@ function App() {
     }
   };
 
-  if (isFetching) return <div>Loading User maps. If this takes a long time the user probably does not have a map saved, I will add error handling later.</div>
+  if (isFetching)
+    return (
+      <div>
+        Loading User maps. If this takes a long time the user probably does not
+        have a map saved, I will add error handling later.
+      </div>
+    );
   return (
-    <div>
+    <div style={{ display: "flex", gap: "32px" }}>
       <div>
-        <h4>Load from:</h4>
-        <label>User Name: </label>
-        <input
-          type="text"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
+        <div>
+          <h4>Load from:</h4>
+          <label>User Name: </label>
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Map Slot: </label>
+          <input
+            type="number"
+            value={mapSlotNum}
+            onChange={(e) => setMapSlotNum(Number(e.target.value))}
+            min="1"
+            max="5"
+          />
+        </div>
+        <div>
+          <label>Server: </label>
+          <select
+            value={loadServer}
+            onChange={(e) => setLoadServer(e.target.value)}
+          >
+            <option value="NA">NA: http://3.145.15.34</option>
+            <option value="EU">EU: http://18.199.164.171</option>
+            <option value="XGEN">XGENSTUDIOS</option>
+          </select>
+        </div>
+        <div>
+          <button onClick={handleLoadClick} disabled={isLoadDisabled}>
+            Load
+          </button>
+        </div>
+        <hr />
+        <h4>Save to:</h4>
+        <div>
+          <label>User Name: </label>
+          <input
+            type="text"
+            value={saveUserName}
+            onChange={(e) => setSaveUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Map Name: </label>
+          <input
+            type="text"
+            value={mapName}
+            onChange={(e) => setMapName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Save Map Slot: </label>
+          <input
+            type="number"
+            value={saveMapSlotNum}
+            onChange={(e) => setSaveMapSlotNum(Number(e.target.value))}
+            min="1"
+            max="5"
+          />
+        </div>
+        <div>
+          <label>Save Server: </label>
+          <select
+            value={saveServer}
+            onChange={(e) => setSaveServer(e.target.value)}
+          >
+            <option value="NA">NA: http://3.145.15.34</option>
+            <option value="EU">EU: http://18.199.164.171</option>
+          </select>
+        </div>
+        <div>
+          <label>Map Data: </label>
+          <textarea
+            placeholder="drag to expand"
+            value={mapData}
+            onChange={(e) => setMapData(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={handleSaveClick} disabled={isSaveDisabled}>
+            Save
+          </button>
+        </div>
       </div>
-      <div>
-        <label>Map Slot: </label>
-        <input
-          type="number"
-          value={mapSlotNum}
-          onChange={(e) => setMapSlotNum(Number(e.target.value))}
-          min="1"
-          max="5"
-        />
-      </div>
-      <div>
-        <label>Server: </label>
-        <select
-          value={loadServer}
-          onChange={(e) => setLoadServer(e.target.value)}
-        >
-          <option value="NA">NA: http://3.145.15.34</option>
-          <option value="EU">EU: http://18.199.164.171</option>
-          <option value="XGEN">XGENSTUDIOS</option>
-        </select>
-      </div>
-      <div>
-        <button onClick={handleLoadClick} disabled={isLoadDisabled}>
-          Load
-        </button>
-      </div>
-      <hr />
-      <h4>Save to:</h4>
-      <div>
-        <label>User Name: </label>
-        <input
-          type="text"
-          value={saveUserName}
-          onChange={(e) => setSaveUserName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Password: </label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Map Name: </label>
-        <input type="text" value={mapName} readOnly />
-      </div>
-      <div>
-        <label>Save Map Slot: </label>
-        <input
-          type="number"
-          value={saveMapSlotNum}
-          onChange={(e) => setSaveMapSlotNum(Number(e.target.value))}
-          min="1"
-          max="5"
-        />
-      </div>
-      <div>
-        <label>Save Server: </label>
-        <select
-          value={saveServer}
-          onChange={(e) => setSaveServer(e.target.value)}
-        >
-          <option value="NA">NA: http://3.145.15.34</option>
-          <option value="EU">EU: http://18.199.164.171</option>
-        </select>
-      </div>
-      <div>
-        <label>Map Data: </label>
-        <textarea
-          placeholder="drag to expand"
-          value={mapData}
-          onChange={(e) => setMapData(e.target.value)}
-        />
-      </div>
-      <div>
-        <button onClick={handleSaveClick} disabled={isSaveDisabled}>
-          Save
-        </button>
+
+      <div hidden={weaponsData.length === 0}>
+        respawn timers 0 - 30 sec:
+        {weaponsData.map((data, i) => (
+          <div key={Math.random()}>
+            weapon {i + 1}:{" "}
+            <input
+              placeholder={`weapon ${i + 1}`}
+              defaultValue={data[4].trim()}
+              type="number"
+              min={0}
+              max={20}
+              onChange={(e) => {
+                const value = parseInt(e.target.value, 10);
+                if (value >= 0 && value <= 30 && e.target.value !== "") {
+                  handleMapDataChange(i, value);
+                }
+              }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
